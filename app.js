9dcf82302325f064b549d5dc32e8d69a0e22e33d
test <qazplm71934682@gmail.com>
2022-07-07 01:29:30 +0800

    [feat] 新增環境相關設定

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const morgan = require('morgan')
+const requestTimeout = function (req, res, next) {
+  res.setTimeout(20 * 1000, () => {
+    console.error(`Response timeout! method:${req.method}, url:${req.originalUrl}, body:${JSON.stringify(req.body, 0, 2)}, ip:${req.ip}`)
+    res.status(500);
+    return res.send()
+  });
+  next();
+};
 app.set('view engine', 'ejs');
-app.use(express.json());
+app.set('case sensitive routing', true);// 路由區分大小寫
+app.set('trust proxy', true); // 取得發送端IP的設定
+
+app.use(express.json())
+app.use(requestTimeout);// 設定超時回應
+app.use(morgan('common', { skip: (req, res) => res.statusCode === 204 }));
+
+app.use((req, res, next) => {
+  // 跨網域設定值
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Max-Age', '1800');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Content-Length, Accept,Authorization');
+  if (req.method === 'OPTIONS') {
+    res.end();
+  } else {
+    next();
+  }
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
